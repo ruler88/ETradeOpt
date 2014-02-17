ce2e1f2e856280cfd687699510990e73f31bd507
ruler88 <helloworld0424@gmail.com>
2014-02-16 20:12:41 -0800

    added json gen by equity name on daily basis

--- src/com/trade/JsonManager/EquityToJSON.java
+++ src/com/trade/JsonManager/EquityToJSON.java
@@ -1,44 +1,86 @@
 package com.trade.JsonManager;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
 
 import org.json.simple.JSONObject;
 
+import com.trade.main.DataAnalysis;
 import com.trade.rowData.DataStorage;
 import com.trade.rowData.Equity;
 
 public class EquityToJSON {
 	
 	public static final String jsonDir = "/mnt/eqJson/";
-	private static final int summarizationFactor = 64;
+	private static final int summarizationFactor = 62;
 
 	public static void main(String[] args) throws IOException {
 		System.out.println("TEST");
-		generateJson("/mnt/tradingData/2014/02/03/dailyMarket20140203-210000");
+		generateJsonRange("20140213", "20140213");
+	}
+	
+	
+	public static void generateJsonRange(String startTime, String endTime) throws IOException {
+		Calendar startDate = Calendar.getInstance();
+		startDate.set(Integer.parseInt(startTime.substring(0, 4)), 
+				Integer.parseInt(startTime.substring(4, 6))-1, 	//minus one 'cause calendar is dumb as shit
+				Integer.parseInt(startTime.substring(6)));
+		
+		Calendar endDate = Calendar.getInstance();
+		endDate.set(Integer.parseInt(endTime.substring(0, 4)), 
+				Integer.parseInt(endTime.substring(4, 6))-1, 
+				Integer.parseInt(endTime.substring(6)));
+		
+		while(startDate.before(endDate) || startDate.equals(endDate)) {
+			File fileDir = new File( DataAnalysis.dataHeadDir + 
+					DataAnalysis.timeFileDir.format(startDate.getTime()) );
+			
+			if(fileDir.isDirectory()) {
+				//if the day exists, will generate
+				List<String> filenames = new ArrayList<String>(DataAnalysis.getDayFile(fileDir));
+				generateJson(filenames, startDate);
+			}
+			startDate.add(Calendar.DATE, 1);
+		}
 	}
 	
 	@SuppressWarnings("unchecked")
-	public static void generateJson(String filename) throws IOException {
-		Hashtable<String, Equity> equityTable = DataStorage.deserializeFile(filename);
-		JSONObject json = new JSONObject();
+	public static void generateJson(List<String> filenames, Calendar date) throws IOException {
+		Hashtable<String, Equity> equityMap = new Hashtable<String, Equity>();
 		
-		for( String s : equityTable.keySet() ) {
-			Equity eq = equityTable.get(s);
-			eq.avgSummarizeEquity(summarizationFactor);
-			json.put( s, makeJsonEquity(eq) );
+		for( int i=0; i<filenames.size(); i++ ) {
+			Hashtable<String, Equity> equityTable = DataStorage.deserializeFile( filenames.get(i) );
+			
+			for( String eq : equityTable.keySet() ) {
+				Equity tmpEq = equityTable.get(eq);
+				tmpEq.avgSummarizeEquity(summarizationFactor);
+				
+				if(equityMap.containsKey(eq)) {
+					equityMap.get(eq).appendEquity(tmpEq);
+				} else {
+					equityMap.put(eq, tmpEq);
+				}
+			}
 		}
 		
-		//TODO: change output name
-		FileWriter file = new FileWriter(jsonDir + DataStorage.dailyMarket);
-		file.write(json.toJSONString());
-		file.flush();
-		file.close();
 		
-		System.out.println(json);
+		for( String eq : equityMap.keySet() ) {
+			JSONObject json = makeJsonEquity(equityMap.get(eq));
+			File outputDir = new File(jsonDir + DataStorage.timeFileDir.format(date.getTime()) + eq);
+			outputDir.getParentFile().mkdirs();
+			FileWriter file = new FileWriter(outputDir);
+			file.write(json.toJSONString());
+			file.flush();
+			file.close();
+			
+			System.out.println("File written to: " + outputDir.toString());
+		}
 	}
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
